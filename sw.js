c6ac6650d7e09f9ec3956030f6b1389c09686047
paleascendancy <rarison1908@gmail.com>
2026-09-09 14:42:06 -0400

    Refresh cache for header menu update

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = "mangamorph-v0.2.9";
+const CACHE = "mangamorph-v0.3.0";
 const ASSETS = [
   "./",
   "./index.html",
   "./manga.html",
-  "./assets/css/style.css?v=024",
+  "./assets/css/style.css?v=025",
   "./assets/css/settings.css?v=001",
   "./assets/css/manga.css?v=006",
-  "./assets/js/app.js?v=017",
+  "./assets/js/app.js?v=018",
   "./assets/js/manga.js?v=005",
   "./manifest.webmanifest"
 ];
