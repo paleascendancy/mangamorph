8a916f2bc17ad3c285eec4698b4d8a92d468ad78
paleascendancy <rarison1908@gmail.com>
2026-09-09 15:59:52 -0400

    Refresh cache for menu account and ranking update

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = "mangamorph-v0.4.2";
+const CACHE = "mangamorph-v0.4.3";
 const ASSETS = [
   "./",
   "./index.html",
   "./manga.html",
-  "./assets/css/style.css?v=032",
+  "./assets/css/style.css?v=033",
   "./assets/css/settings.css?v=002",
   "./assets/css/manga.css?v=006",
-  "./assets/js/app.js?v=026",
+  "./assets/js/app.js?v=027",
   "./assets/js/manga.js?v=005",
   "./manifest.webmanifest"
 ];
