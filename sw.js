95111c9c1bdedece21d1eee31e5141b58ab18c51
paleascendancy <rarison1908@gmail.com>
2026-09-09 15:19:48 -0400

    Refresh cache for account hub redesign

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = "mangamorph-v0.3.6";
+const CACHE = "mangamorph-v0.3.7";
 const ASSETS = [
   "./",
   "./index.html",
   "./manga.html",
-  "./assets/css/style.css?v=031",
+  "./assets/css/style.css?v=032",
   "./assets/css/settings.css?v=001",
   "./assets/css/manga.css?v=006",
-  "./assets/js/app.js?v=021",
+  "./assets/js/app.js?v=022",
   "./assets/js/manga.js?v=005",
   "./manifest.webmanifest"
 ];
