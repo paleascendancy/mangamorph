42f3eaef3585737a445506d8d8ffd7e220d2021f
paleascendancy <rarison1908@gmail.com>
2026-09-09 16:19:43 -0400

    Refresh cache for side menu refinement

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = "mangamorph-v0.4.5";
+const CACHE = "mangamorph-v0.4.6";
 const ASSETS = [
   "./",
   "./index.html",
   "./manga.html",
-  "./assets/css/style.css?v=035",
+  "./assets/css/style.css?v=036",
   "./assets/css/settings.css?v=002",
   "./assets/css/manga.css?v=006",
-  "./assets/js/app.js?v=028",
+  "./assets/js/app.js?v=029",
   "./assets/js/manga.js?v=005",
   "./manifest.webmanifest"
 ];
