4d6784a647760da0083950a41946793cb5cc8feb
paleascendancy <rarison1908@gmail.com>
2026-09-09 14:50:30 -0400

    Refresh cache for account panel

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = "mangamorph-v0.3.2";
+const CACHE = "mangamorph-v0.3.3";
 const ASSETS = [
   "./",
   "./index.html",
   "./manga.html",
-  "./assets/css/style.css?v=027",
+  "./assets/css/style.css?v=028",
   "./assets/css/settings.css?v=001",
   "./assets/css/manga.css?v=006",
-  "./assets/js/app.js?v=018",
+  "./assets/js/app.js?v=019",
   "./assets/js/manga.js?v=005",
   "./manifest.webmanifest"
 ];
