8b92a2ff1fc098e68df0de55e297dd7bce94c996
paleascendancy <rarison1908@gmail.com>
2026-09-09 13:50:37 -0400

    chore: refresh cache for chapter sort toggle

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE = "mangamorph-v0.1.8";
+const CACHE = "mangamorph-v0.1.9";
 const ASSETS = [
   "./",
   "./index.html",
   "./manga.html",
   "./assets/css/style.css?v=017",
-  "./assets/css/manga.css?v=003",
+  "./assets/css/manga.css?v=004",
   "./assets/js/app.js?v=015",
-  "./assets/js/manga.js?v=003",
+  "./assets/js/manga.js?v=004",
   "./manifest.webmanifest"
 ];
 
