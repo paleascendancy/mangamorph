df2ce2a5ceab0dcc6fc4287872e45721d19aa184
paleascendancy <rarison1908@gmail.com>
2026-09-09 16:38:32 -0400

    Refresh cache for chapter list refinement

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE = "mangamorph-v0.5.0";
+const CACHE = "mangamorph-v0.5.1";
 const ASSETS = [
   "./",
   "./index.html",
   "./manga.html",
   "./assets/css/style.css?v=038",
   "./assets/css/settings.css?v=002",
-  "./assets/css/manga.css?v=007",
+  "./assets/css/manga.css?v=008",
   "./assets/js/app.js?v=032",
-  "./assets/js/manga.js?v=006",
+  "./assets/js/manga.js?v=007",
   "./manifest.webmanifest"
 ];
 
