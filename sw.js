56d9f4ef27b234a670a1d73e93a27467709f5aa1
paleascendancy <rarison1908@gmail.com>
2026-09-09 16:11:12 -0400

    Refresh cache for compact account redesign

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "mangamorph-v0.4.3";
+const CACHE = "mangamorph-v0.4.4";
 const ASSETS = [
   "./",
   "./index.html",
   "./manga.html",
-  "./assets/css/style.css?v=033",
+  "./assets/css/style.css?v=034",
   "./assets/css/settings.css?v=002",
   "./assets/css/manga.css?v=006",
   "./assets/js/app.js?v=027",
