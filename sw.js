44e6607b58b12eed6361e0180b2208f68465f3ab
paleascendancy <rarison1908@gmail.com>
2026-09-09 17:19:42 -0400

    Cache authentication assets

--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
-const CACHE = "mangamorph-v0.7.0";
+const CACHE = "mangamorph-v0.8.0";
 const ASSETS = [
   "./",
   "./index.html",
   "./manga.html",
   "./reader.html",
   "./assets/css/style.css?v=039",
+  "./assets/css/auth.css?v=001",
   "./assets/css/settings.css?v=002",
   "./assets/css/manga.css?v=013",
   "./assets/css/reader.css?v=001",
-  "./assets/js/app.js?v=033",
+  "./assets/js/app.js?v=034",
+  "./assets/js/auth.js?v=001",
   "./assets/js/manga.js?v=010",
   "./assets/js/reader.js?v=001",
   "./manifest.webmanifest"
