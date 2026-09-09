9e6dc50fcd578345fa597b7d51c7d8e5f49eacc8
paleascendancy <rarison1908@gmail.com>
2026-09-09 15:36:56 -0400

    Refresh cache for type filter update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mangamorph-v0.3.9";
+const CACHE = "mangamorph-v0.4.0";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,7 +6,7 @@ const ASSETS = [
   "./assets/css/style.css?v=032",
   "./assets/css/settings.css?v=001",
   "./assets/css/manga.css?v=006",
-  "./assets/js/app.js?v=024",
+  "./assets/js/app.js?v=025",
   "./assets/js/manga.js?v=005",
   "./manifest.webmanifest"
 ];
