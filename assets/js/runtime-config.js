35e221be9c5267bd8d9c311124804002951d55e5
paleascendancy <rarison1908@gmail.com>
2026-09-13 00:04:40 -0400

    refactor: add shared runtime config

---
+++ assets/js/runtime-config.js
@@ -0,0 +1,3 @@
+export const MANGAMORPH_VERSION="1.1.0";
+export const SUPABASE_URL="https://fnyellunugdfesprmvzm.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY="sb_publishable_clf6HlhhxdftO1_XZU7YsA_pRmkCEJK";
