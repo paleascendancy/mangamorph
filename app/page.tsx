ec156d8235513d7940a6d627ef3df8b734a9cc1c
paleascendancy <rarison1908@gmail.com>
2026-09-18 23:52:25 -0400

    feat: add empty home sections structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { Hero } from '../components/home/Hero';
+import { HomeSections } from '../components/home/HomeSections';
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <HomeSections />
+    </>
+  );
 }
